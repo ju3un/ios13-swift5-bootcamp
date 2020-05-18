2e0d514bf460ac0d1a3dcb9f3bd4cbfa018b8c8a
ju3un <ju3un@ju3unui-MacBookPro.local>
2020-05-18 15:16:16 +0900

    [add] by Section4

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,8 +7,20 @@ var b = 8
 print("The value of a is \(a)")
 print("The value of b is \(b)")
 
+//
+
 var numbers = [45, 73, 195, 53]
 
 var computedNumbers = [45*73, 73*195, 195*53]
 
 print(computedNumbers)
+
+//
+
+let randomNumber = Int.random(in: 1 ..< 3) // 1, 2 random
+let randomNumber2 = Float.random(in: 1 ..< 3)
+/*
+ Bool.random()
+ Array.randomElement()
+ Array.shuffle()
+ */
