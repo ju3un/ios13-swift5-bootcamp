99116647c99fb766266b89ff4fbe4b07d4783e04
ju3un <ju3un@ju3unui-MacBookPro.local>
2020-05-19 17:46:23 +0900

    [add] ing Section8

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,27 +1,15 @@
 
 
-func loveCalculator() {
-    
-    let loveScore = Int.random(in: 0...100)
-    
-//    if loveScore > 80 {
-//        print("You love each other like Kanye loves Kanye")
-//    } else if loveScore < 40 {
-//        print("You go. ogether like Coke and Mentos")
-//    } else {
-//        print("You'll be forever alone")
-//    }
-    
-    switch loveScore {
-    case 81...100:
-        print("You love each other like Kanye loves Kanye")
-    case 41...80:
-        print("You go. ogether like Coke and Mentos")
-    case ...40:
-        print("You'll be forever alone")
-    default:
-        print("Error")
-    }
-}
+//var player1Username: String = nil
 
-loveCalculator()
+var player1Usernamer: String? = nil
+
+player1Usernamer = "jack"
+
+var unwrappedP1Username = player1Usernamer!
+
+player1Usernamer = nil
+
+if player1Usernamer != nil {
+    print(player1Usernamer!)
+}
