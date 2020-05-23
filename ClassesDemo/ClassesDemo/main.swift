bf6584da46c6d50e2a97dd265771110c282b4d13
ju3un <ju3un@ju3unui-MacBookPro.local>
2020-05-23 18:29:06 +0900

    [add] -ing section 11

---
+++ ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,18 @@
+
+let skeleton = Enemy(health: 100, attackStrength: 10)
+//print(skeleton.health)
+//skeleton.move()
+//skeleton.attack()
+
+//let skeleton2 = skeleton
+let skeleton2 = Enemy(health: 100, attackStrength: 10)
+
+skeleton.takeDamage(amount: 10)
+print(skeleton2.health)
+
+//let dragon = Dragon()
+//dragon.wingSpan = 5
+//dragon.attackStrength = 15
+//dragon.talk(speech: "Merong")
+//dragon.move()
+//dragon.attack()
