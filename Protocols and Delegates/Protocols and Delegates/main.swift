1cc655449b2801b12d0f3a8720f022015766627b
ju3un <ju3un@ju3unui-MacBookPro.local>
2020-05-27 01:44:16 +0900

    [add] -ing section 13

---
+++ Protocols and Delegates/Protocols and Delegates/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Protocols and Delegates
+//
+//  Created by ju3un on 2020/05/26.
+//  Copyright © 2020 ju3un. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by ju3un on 2020/05/26.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
