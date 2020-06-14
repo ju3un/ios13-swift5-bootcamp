e87476c871536d7cb23ab2d1ebf0089161d2913f
ju3un <ju33un@gmail.com>
2020-06-14 20:41:47 +0900

    [add] by section 19 : Core Data

---
+++ CoreDataTest/CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by ju3un on 2020/06/10.
+//  Copyright © 2020 ju3un. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
